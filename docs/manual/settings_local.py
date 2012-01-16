add6d67f35a2f053fecbbd1ee16e37cca78395ea
Shaik Parvez <parvezx.shaik@intel.com>
2012-01-16 15:59:13 +0200

    docs added

---
+++ docs/manual/settings_local.py
@@ -0,0 +1,15 @@
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'docs.db',
+    }
+}
+
+SECRET_KEY = 'HASJFDYWQ98r6y2hesakjfhakjfy87eyr1hakjwfa'
+CACHE_BACKEND = 'locmem://'
+LOCAL_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data'))
+ADMINS = [
+    ('Example Admin', 'admin@example.com'),
+]
